78668230df02802249850c8afd0762724fb7aa06
Camilo Andrés Carranza <79939575+ccarranzacSlab@users.noreply.github.com>
2021-08-25 17:27:04 -0500

    Update vue-apollo.js

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -9,7 +9,7 @@ Vue.use(VueApollo)
 const AUTH_TOKEN = 'apollo-token'
 
 // Http endpoint
-const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'http://localhost:5000/graphql'
+const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'http://35.226.14.189/graphql'
 
 // Config
 const defaultOptions = {
